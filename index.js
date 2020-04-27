f84e6735dfbd48d173618a02d6ef15ceab84c52b
linlinliu <linlinliu03@gmail.com>
2020-04-26 23:18:46 -0700

    Set static file

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var express = require('express');
 
 var app = express();
 
+app.use(express.static('public'));
+
 
 var server = app.listen(4000, ()=>{
     console.log("Listening request on port 4000!")
 })
+
