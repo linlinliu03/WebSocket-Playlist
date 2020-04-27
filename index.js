30fa970ed4aed22eebd34db77c94eedd8ea99d0c
linlinliu <linlinliu03@gmail.com>
2020-04-27 00:51:15 -0700

    Broadcast chat

--- index.js
+++ index.js
@@ -21,4 +21,9 @@ io.on("connection", (socket) => {
         // console.log(data);
         io.sockets.emit('chat', data);
     });
+
+    // Handle typing event
+    socket.on('typing', function (data) {
+        socket.broadcast.emit('typing', data);
+    });
 })
--- public/chat.js
+++ public/chat.js
@@ -15,7 +15,16 @@ btn.addEventListener('click', function () {
     message.value = "";
 });
 
+message.addEventListener('keypress', function () {
+    socket.emit('typing', handle.value);
+})
+
 // Listen for events
 socket.on('chat', function (data) {
+    feedback.innerHTML = '';
     output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+});
+
+socket.on('typing', function (data) {
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
 });
